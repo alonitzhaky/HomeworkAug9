c3a9ac692a9dc960f27964aaf7515750353338b5
alon.itzhaky <alon.itzhaky@gmail.com>
2022-08-09 22:09:30 +0300

    Homework Changes

--- homeworkfix.js
+++ homeworkfix.js
@@ -2,9 +2,9 @@
 function doSomeCalc() {
     console.log("i did math yay")
 }
-function yayForMe() {
-    console.log("yay for you")
-}   
+// function yayForMe() {
+//     console.log("yay for you")
+// }   
 {/* Mission 2 */}
 function doCombo() {
     console.log("i hate JavaScript")
