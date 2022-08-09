5b7adbeb106cf9cbc02616b9d30cd832951989e9
alon.itzhaky <alon.itzhaky@gmail.com>
2022-08-09 21:12:41 +0300

    first commit

---
+++ homeworkfix.js
@@ -0,0 +1,11 @@
+{/* Mission 1 */}
+function doSomeCalc() {
+    console.log("i did math yay")
+}
+function yayForMe() {
+    console.log("yay for you")
+}   
+{/* Mission 2 */}
+function doCombo() {
+    console.log("i hate JavaScript")
+}
